c5b0051547c284d4ba1f1de62322dfa0796c5135
Yavor Ivanov <yavor@gmail.com>
2015-05-12 19:06:44 +0300

    Moving data to json and applying it using angular

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('htmlCV', []);
+
+app.controller('cvController', ['$http', function($http) {
+  var ctrl = this;
+  ctrl.cvItems = {};
+  $http.get('data.json').success(function(data) {
+    ctrl.cvItems = data;
+  });
+  this.shortName = function() {
+    return this.cvItems.firstName + ' ' + this.cvItems.lastName;
+  };
+  this.fullName = function() {
+    return this.cvItems.firstName + ' ' + this.cvItems.middleName +
+      ' ' + this.cvItems.lastName;
+  };
+  
+  this.skillsetMap = {
+    1: "Theoretical Knowledge",
+    2: "Some Experience",
+    3: "Significant Experience",
+    4: "Expertise",
+    5: "Advanced Skills"
+  };
+  this.skillsetPhonetic = function(skillset) {
+    return this.skillsetMap[skillset];
+  };
+}]);
+
+app.filter('rawHtml', ['$sce', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
+}]);
